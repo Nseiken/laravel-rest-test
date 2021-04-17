9ce55a28333eefdc561ffb9d7148695f8b12e04f
Cristian Garcia <ncgarcias@hotmail.com>
2021-04-17 07:39:58 -0500

    add book resource and collection

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'book_title' => $this->book_name,
+            'cover_url' => $this->cover_url,
+            'author' => $this->author->author_name,
+            'created' => $this->created_at
+        ];
+    }
+}
