3908212c9796a9ed8c2762707047d25ce8148703
Cristian Garcia <ncgarcias@hotmail.com>
2021-04-20 08:33:13 -0500

    install laravel jetstream

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.web')
+
+@section('content')
+    <div class="text-center">
+        <h1 class="text-3xl text-grat-700 mb-2 uppercase">Listado de libros</h1>
+        <h2 class="text-xl text-gray-600">Vamos a cargar desde el Api</h2>
+    </div>
+@endsection
